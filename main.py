4d01c8cdb8bc1c3acc00de673341c3f3cd24a5f0
camden.warburton <camden.warburton@ucalgary.ca>
2026-09-23 17:34:08 -0600

    Made some bug fixes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
 
     url = "https://api.sunrise-sunset.org/v2?lat=36.7201600&lng=-4.4203400&date=2026-09-22"
     print("--------")
-    print(getBound(m_lat=51.136258,m_long=-114.182517, distance=5))
+    print(getBound(m_lat=51.136258,m_long=-114.182517, distance=0.5))
 
 if __name__ == "__main__":
     main()
--- maps/coordinateCalculator.py
+++ maps/coordinateCalculator.py
@@ -1,16 +1,18 @@
 import math
 
-def calcLong(lat:float,distance:float):
-    return (distance/(6371 * math.cos(lat))) * (180.0/math.pi)
-
+def calcLong(lat: float, distance: float):
+    return (distance / (6371 * math.cos(math.radians(lat)))) * (180 / math.pi)
 
 # Gets the perimeter coordinates, distance needs to be in km
-def getBound(m_long:float,m_lat:float, distance:int):
-
-    distance = float(distance)
+def getBound(m_long: float, m_lat: float, distance: float):
     lat_change = distance / 111.19
+
     south = m_lat - lat_change
     north = m_lat + lat_change
-    west = m_long - calcLong(south,distance)
-    east = m_long + calcLong(north,distance)
-    return south,west,north,east
+
+    long_change = calcLong(m_lat, distance)
+
+    west = m_long - long_change
+    east = m_long + long_change
+
+    return south, west, north, east
