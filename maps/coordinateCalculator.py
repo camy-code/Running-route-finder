c589c97cb4777dfa0288b187695f927a247fab77
camden.warburton <camden.warburton@ucalgary.ca>
2026-09-23 23:32:56 -0600

    made the haversine formula

--- maps/coordinateCalculator.py
+++ maps/coordinateCalculator.py
@@ -1,4 +1,5 @@
 import math
+from haversine import haversine, Unit
 
 def calcLong(lat: float, distance: float):
     return (distance / (6371 * math.cos(math.radians(lat)))) * (180 / math.pi)
@@ -16,3 +17,8 @@ def getBound(m_long: float, m_lat: float, distance: float):
     east = m_long + long_change
 
     return south, west, north, east
+
+
+# This method will return the distance of 2 points in km
+def calcDistance(old_lat,old_long,new_lat,new_long):
+    return haversine((old_lat,old_long),(new_lat,new_long))
